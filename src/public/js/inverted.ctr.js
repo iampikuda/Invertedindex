8a1b4c8cbd8fec8057500a9a6a634281e3c8b982
andela-opikuda <oluwadamisi.pikuda@andela.com>
2017-02-23 10:03:41 +0100

    chore(update): solving the problem

--- src/public/js/inverted.ctr.js
+++ src/public/js/inverted.ctr.js
@@ -1,30 +1,55 @@
-(function(){
+(function () {
 	angular.module('ngInverted', [])
-		.controller('UploadFile',['$scope','$http', ($scope, $http) => {
+		.controller('UploadFile', ['$scope', '$http', ($scope, $http) => {
+			const titleArray = {};
+			const textArray = {};
 
-		const onFileComplete = (response) => {
-			$scope.user = response.data;
-			
-		};
+			const onFileComplete = (response) => {
+				let upload = response.data;
+				let count = 1;
 
-		const onError = (reason) => {
-			$scope.error = "Not valid json";
-		};
-		
-		$http.get("https://gist.githubusercontent.com/q-ode/72019451b98f079a8d737eb7a412bf14/raw/bd3b75398c2f95adfef10f48f43c36475e950890/books.json")
-					.then(onFileComplete, onError);
-		// const onJson = (response) => {
-		// 	$scope.title = response.data;
-		// };
+				function transforming(file){
+					for(let words in file){
+						file[words] = file[words].replace(/'\w+\s/g," ").replace(/[.,/#!+$%^&@*?;:'{}=\-_`~()]/g, '').trim().toLowerCase().split(' ')
+						 
+					}
+					return file;
+				};
+				// .replace(/'\w+\s/g," ").replace(/[.,/#!+$%^&@*?;:'{}=\-_`~()]/g, '').trim().toLowerCase().split(' ')
 
-		
+				
+				for (let batter in upload) {
+					titleArray[count] = upload[batter].title
+					textArray[count] = upload[batter].text
+					console.log(titleArray);
+					count++
+				};
 
-		
-					
-		
+				transforming(textArray);
+				console.log(textArray);
 
-	}]);
+				$scope.pasta = titleArray;
 
-	
+			};
+
+			const onError = (reason) => {
+				$scope.error = "Not valid json";
+			};
+
+			$http.get("https://gist.githubusercontent.com/q-ode/72019451b98f079a8d737eb7a412bf14/raw/bd3b75398c2f95adfef10f48f43c36475e950890/books.json")
+				.then(onFileComplete, onError);
+			// const onJson = (response) => {
+			// 	$scope.title = response.data;
+			// };
+
+
+
+
+
+
+
+		}]);
+
+
 
 }());
