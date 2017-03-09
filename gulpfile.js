ce6b153d742d8d709ba5fada58c89ced0f68d21c
andela-opikuda <oluwadamisi.pikuda@andela.com>
2017-03-09 18:05:40 +0100

    setup testing environment and coverage

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,12 @@
 const gulp = require('gulp');
 const browserSync = require('browser-sync').create();
+const Server = require('karma').Server;
+const path = require('path');
 
 gulp.task('serve', () => {
   browserSync.init({
     server: {
-      baseDir: './',
+      baseDir: './src',
       index: 'src/views/index.html'
     },
     options: {
@@ -24,4 +26,11 @@ gulp.task('default', ['serve', 'refresh'], () => {
   gulp.watch('./src/public/**/*.js', ['refresh']);
   gulp.watch('./*.js', ['refresh']);
   gulp.watch('./src/public/css/*.css', ['refresh']);
+});
+
+gulp.task('test', (done) => {
+  new Server({
+    configFile: path.join(__dirname, 'karma.conf.js'),
+    singleRun: true
+  }, done).start();
 });
---
+++ jasmine/spec/inverted-index-test.js
@@ -0,0 +1,12 @@
+
+const invertedIndex = new InvertedIndex();
+
+describe('Inverted Index Test', () => {
+  describe('It Validate Book Data', () => {
+    it('should return invalid file type if book is not an array of objects',
+     () => {
+       expect(true).toBe(true);
+     });
+  });
+});
+
